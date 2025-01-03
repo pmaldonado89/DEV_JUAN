0ae6dcf5da54ea4133f20c9c2b1587c38d9c6b51
RDuuke90 <115588732+RDuuke90@users.noreply.github.com>
2025-01-03 08:56:35 -0500

    Delete src directory

--- src/interfaces/Miner.ts
+++
@@ -1,7 +0,0 @@
-export interface Miner {
-    id: string;
-    full_name: string;
-    id_type: string;
-    id_number: string;
-    municipality: string;
-}
--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home: React.FC = () => {
-  return <h1>Bienvenido a la Gestión de Mineros</h1>;
-};
-
-export default Home;
