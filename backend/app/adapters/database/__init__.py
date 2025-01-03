ad0c91f53bfd15e20c68b2f8f8e98b646180cb33
RDuuke90 <115588732+RDuuke90@users.noreply.github.com>
2025-01-03 09:08:24 -0500

    Update __init__.py

--- backend/app/adapters/database/__init__.py
+++ backend/app/adapters/database/__init__.py
@@ -1,2 +1,2 @@
-from app.adapters.database.model import MinerRepositoryPort
-from app.adapters.database import miners
+from app.adapters.database.model import miners
+from app.adapters.database.repository import MinerRepositoryPort
