aa6e057c60864294a10c4ecbb5dc5e1beb711603
RDuuke90 <115588732+RDuuke90@users.noreply.github.com>
2025-01-03 08:25:24 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from app.config  import lifespan
-from app.adapters.api import api_router
-
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(api_router, prefix="/api/v1")
